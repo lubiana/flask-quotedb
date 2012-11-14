f71cbd6149f0d7ccece0d36d17107a8fe2f42c08
Lubian <lubiana123@gmail.com>
2012-11-14 22:36:36 +0100

    solved bug in the admin_required decorator

--- app/decorators.py
+++ app/decorators.py
@@ -1,11 +1,14 @@
 from functools import wraps
 from flask import g, request, redirect, url_for, flash
-from models import ROLE_ADMIN, User
+from models import User
 
 def admin_required(f):
 	@wraps(f)
 	def decorated_function(*args, **kwargs):
-		if g.user.role != ROLE_ADMIN:
+		if g.user.is_anonymous():
+			flash('user is anonymous')
+			return redirect(url_for('index'))
+		if not g.user.is_admin():
 			flash('not authorized')
 			return redirect(url_for('index'))
 		return f(*args, **kwargs)
